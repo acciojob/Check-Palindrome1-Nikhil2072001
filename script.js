120fb73d653a4e0e86e59425edabfef5cfc89f71
Nikhil2072001 <116430126+Nikhil2072001@users.noreply.github.com>
2023-03-18 20:29:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 // complete the given function
 
 function palindrome(str){
- const string = str.toLowerCase();
- const len = str.length;
-
-    // loop through half of the string
-    for (let i = 0; i < len / 2; i++) {
-
-        // check if first and last string are same
-        if (string[i] !== string[len - 1 - i]) {
-            return false;
-        }
-    }
-    return true;
+// str = str.split(" ","");
+	str=str.toLowerCase();
+	let n = str.length();
+	for(let i=0;i<n/2;i++){
+		if(str.charAt(i)!=str.charAt(n-1-i))
+			return false;
+	}
+	return true;
 }
 module.exports = palindrome
+//  const string = str.toLowerCase();
+//  const len = str.length;
+
+//     // loop through half of the string
+//     for (let i = 0; i < len / 2; i++) {
+
+//         // check if first and last string are same
+//         if (string[i] !== string[len - 1 - i]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// module.exports = palindrome
